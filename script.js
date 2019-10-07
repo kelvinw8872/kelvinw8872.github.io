53550f1caa4801031178e42345d87d700c7c0c7f
kelvinw8872 <43858639+kelvinw8872@users.noreply.github.com>
2019-10-07 10:25:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Kelvin W."
+let userName = "kelvinw8872";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
